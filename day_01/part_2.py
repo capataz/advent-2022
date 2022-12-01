e0ac412718d7123d980bd816417dc51dcf79b651
Justin Sheen <justinsheen2@gmail.com>
2022-12-01 17:48:45 -0500

    clean

--- day_01/part_2.py
+++ day_01/part_2.py
@@ -1,20 +1,24 @@
+from types import SimpleNamespace
+
+def process_line(input, v):
+    if len(input):
+        v.total_cal += int(input)
+    else:
+        v.cals.append(v.total_cal)
+        v.total_cal = 0
 
 def main():
-    cals = []
-    total_cal = 0
+    v = SimpleNamespace(**{
+        'cals': [],
+        'total_cal': 0
+    })
 
     with open('./day_01/puzzle.dat') as f:
-        for ln in f:
-            ln = ln.strip()
-            if len(ln):
-                total_cal += int(ln)
-            else:
-                cals.append(total_cal)
-                total_cal = 0
+        [process_line(ln.strip(), v) for ln in f]
 
-    cals.sort()
+    v.cals.sort()
 
-    return sum(cals[-3:])
+    return sum(v.cals[-3:])
 
 
 if __name__ == '__main__':
