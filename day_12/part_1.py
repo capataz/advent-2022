25a440ed33574db5cdf7a029993f152184561d22
Justin Sheen <justinsheen2@gmail.com>
2022-12-13 08:27:24 -0500

    part 1 to queue

--- day_12/part_1.py
+++ day_12/part_1.py
@@ -1,53 +1,43 @@
 import numpy as np
 import string
+from collections import deque
 
 # F = './day_12/test.dat'
 F = './day_12/puzzle.dat'
 
+def process(value, x, y, max_x, max_y, visited, mp):
+    if x >= max_x or y >= max_y or x < 0 or y < 0 or \
+        value < mp[x][y] - 1 or \
+        (x, y) in visited:
 
-def mark(current_val, x, y, count, max_x, max_y, visited, mp):
-    if x >= max_x or y >= max_y or x < 0 or y < 0:
-        # Out of grid bounds
-        return
+        return False
 
-    if visited[x][y] and visited[x][y] < count:
-        # Already visited on a shorter path
-        return
-
-    if current_val < mp[x][y] - 1:
-        # Too tall to move
-        return
-
-    visited[x][y] = count
+    return True
 
 
 def main():
     mp = np.array([[string.ascii_letters.index(c) for c in t]
         for t in [list(x) for x in [l.strip() for l in open(F)]]])
+
     x, y = [c[0] for c in np.where(mp == 44)]
     target_x, target_y = [c[0] for c in np.where(mp == 30)]
-    max_x = len(mp)
-    max_y = len(mp[0])
-
     mp[target_x][target_y] = string.ascii_letters.index('z') # override height
-    mp[x][y] = 0
+    mp[x][y] = string.ascii_letters.index('a') # override height
 
-    visited = np.zeros_like(mp)
-    visited[x][y] = 1
-
-    step = 1
+    q = deque([[x, y, 1]])
+    visited = set((x, y))
     while True:
-        for i, row in enumerate(visited):
-            for j, val in enumerate(row):
-                if val == step:
-                    if i == target_x and j == target_y:
-                        return step - 1
+        x, y, count = q.popleft()
 
-                    mark(mp[i][j], i+1, j, step + 1, max_x, max_y, visited, mp)
-                    mark(mp[i][j], i-1, j, step + 1, max_x, max_y, visited, mp)
-                    mark(mp[i][j], i, j+1, step + 1, max_x, max_y, visited, mp)
-                    mark(mp[i][j], i, j-1, step + 1, max_x, max_y, visited, mp)
-        step += 1
+        if x == target_x and y == target_y:
+            return count - 1
+
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            new_x = x + dx
+            new_y = y + dy
+            if process(mp[x][y], new_x, new_y, len(mp), len(mp[0]), visited, mp):
+                q.append([new_x, new_y, count + 1])
+                visited.add((new_x, new_y))
 
 
 if __name__ == '__main__':
@@ -55,4 +45,4 @@ if __name__ == '__main__':
     print(answer)
     assert answer == 484
 
-# 1702
+# 484
